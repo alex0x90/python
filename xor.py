e14c7c4191e07248acef936af8854e2970de97e0
Alex <magsk8@yandex.com>
2020-04-18 20:27:01 +0300

    Create xor.py

---
+++ xor.py
@@ -0,0 +1,9 @@
+message = list(input("Write the message: "))
+key = input("Write the key: ")
+
+for symbol in range(len(message)):
+    try: 
+        message[symbol] = chr(ord(message[symbol]) ^ int(key))
+    except ValueError: 
+        message[symbol] = chr(ord(message[symbol]) ^ ord(key))
+
